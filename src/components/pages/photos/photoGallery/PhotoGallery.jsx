32cc1e09cf6b37a2dd6642f0d658ec7335f388e4
lerixxi <vchumack2ne1@gmail.com>
2023-06-18 00:49:40 +0300

    added slider in Photogallery section

--- src/components/pages/photos/photoGallery/PhotoGallery.jsx
+++ src/components/pages/photos/photoGallery/PhotoGallery.jsx
@@ -1,75 +1,69 @@
 import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import { photos } from "@/data/photosPage/gallery";
+import { MdArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md";
 import styles from "./PhotoGallery.module.scss";
 
 export const PhotoGallery = () => {
-	const [isOpen, setIsOpen] = useState(false);
-	const [photoIndex, setPhotoIndex] = useState(0);
+	const [current, setCurrent] = useState(0);
 
-	const images = photos.map((photo) => photo.src);
+	const timeoutRef = useRef(null);
 
-	const openLightbox = (index) => {
-		setPhotoIndex(index);
-		setIsOpen(true);
-		document.body.style.overflow = "hidden"; // prevent scrolling when the lightbox is open
+	function resetTimeout() {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
+		}
+	}
+
+	useEffect(() => {
+		resetTimeout();
+		timeoutRef.current = setTimeout(
+			() =>
+				setCurrent((prevIndex) =>
+					prevIndex === photos.length - 1 ? 0 : prevIndex + 1
+				),
+			3000
+		);
+
+		return () => {
+			resetTimeout();
+		};
+	}, [current]);
+
+	const nextSlide = () => {
+		setCurrent(current === photos.length - 1 ? 0 : current + 1);
 	};
 
-	const closeLightbox = () => {
-		setIsOpen(false);
-		document.body.style.overflow = "unset"; // enable scrolling again when the lightbox is closed
+	const prevSlide = () => {
+		setCurrent(current === 0 ? photos.length - 1 : current - 1);
 	};
 
 	return (
-		<section>
+		<section className={styles.section}>
 			<div className={`container ${styles.container}`}>
-				<div className={styles.gallery}>
+				<button onClick={prevSlide}>
+					<MdArrowBackIos />
+				</button>
+				<ul className={styles.slider}>
 					{photos.map((photo, index) => (
-						<div key={index} onClick={() => openLightbox(index)}>
+						<li
+							key={index}
+							className={`${styles.slide} ${
+								index === current ? styles.active : ""
+							}`}
+						>
 							<Image
 								src={photo.src}
 								alt={photo.alt}
 								width={photo.width}
 								height={photo.height}
 							/>
-						</div>
+						</li>
 					))}
-				</div>
-				{isOpen && (
-					<div className={styles.lightbox}>
-						<span className={styles.close} onClick={closeLightbox}>
-							&times;
-						</span>
-						<Image
-							className={styles.lightboxImg}
-							src={images[photoIndex]}
-							alt="Gallery"
-							width={700}
-							height={481}
-						/>
-						<div className={styles.lightboxContent}>
-							<button
-								onClick={() =>
-									setPhotoIndex(
-										(photoIndex + images.length - 1) %
-											images.length
-									)
-								}
-							>
-								Prev
-							</button>
-							<button
-								onClick={() =>
-									setPhotoIndex(
-										(photoIndex + 1) % images.length
-									)
-								}
-							>
-								Next
-							</button>
-						</div>
-					</div>
-				)}
+				</ul>
+				<button onClick={nextSlide}>
+					<MdOutlineArrowForwardIos />
+				</button>
 			</div>
 		</section>
 	);
